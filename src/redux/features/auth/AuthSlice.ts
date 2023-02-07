b0fcdbbb66cf1be551a9c42114a6bca29c70639c
leroy-netizen <leroythomas051@gmail.com>
2023-02-07 09:30:25 +0300

    feat(auth slice): set up the auth slice

---
+++ src/redux/features/auth/AuthSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  token: null,
+};
+
+const AuthSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setCreds: (state, action) => {
+      const { user, accessToken } = action.payload;
+      (state.user = user), (state.token = accessToken);
+    },
+    logOut: (state, action) => {
+      (state.user = null), (state.token = null);
+    },
+  },
+});
+
+export const { setCreds, logOut } = AuthSlice.actions;
+export default AuthSlice.reducer;
